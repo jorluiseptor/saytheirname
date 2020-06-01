1b23e2d59f9913ea629451677fcb0cf4e65b022f
Jorge Sepulveda <jorluiseptor@gmail.com>
2020-05-31 22:27:28 -0400

    Add github pages.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as data from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  people:any = (data as any).default; 
 }
